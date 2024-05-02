cd6fb3981a74629231870968ca3b0d70726e7002
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-05-02 10:56:05 -0500

    more done

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "The Coolest Books Around";
+   let book1 = "https://www.shambhala.com/media/catalog/product/cache/a97331a5130f36fd8e47c3963d3aaa32/9/7/9781590304488_1.png";
+   let book2 = "https://pictures.abebooks.com/inventory/15698731412.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/61JE7tWDvqL._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Art of Peace" />
+         <img src={book2} alt="Inherent Vice" width="160px"/>
+         <img src={book3} alt="No Bad Parts" width="200px"/>
       </div>      
    );
 }
