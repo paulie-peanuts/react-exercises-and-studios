e71e4ce227d04202ab99e9042522fae231b2934e
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-05-06 10:36:16 -0500

    part 3

--- part2/exercises-2/src/components/Profile.jsx
+++ part2/exercises-2/src/components/Profile.jsx
@@ -1,15 +1,14 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
-
-function Profile()
-{
+ function Profile()
+ {
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
          <Button />
       </>  
    );
-}
+ }
 
 export default Profile;
