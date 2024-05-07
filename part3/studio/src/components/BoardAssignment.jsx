9af17a3c157d2b07c68a4f831f393068e0404f4d
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-05-07 14:59:34 -0500

    studio done

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const [boardName, setBoardName] = useState('no boards yet!');
+   const boards = [
+      {label: 'Bad Recipes', value:'badRecipes'},
+      {label: 'Scary Mad Women', value:'scaryMadWomen'},
+      {label: 'Crack and Cheese', value:'crackandCheese'},
+   ];
 
    const handleChange = (event) => {
+      setBoardName(event.target.value)
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boards.map((board) => {
+            return (
+               <option key={board.label} value={board.value}>{board.label}</option>
+            )
+         })}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://thecozycook.com/stovetop-mac-and-cheese/";
+   let authorPhoto = "https://thecozycook.com/wp-content/uploads/2021/11/The-Cozy-Cook.jpg";
+   let authorName = "Stephanie Melchione";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = {authorName} style={{objectFit: "contain", borderRadius: "50%", width:"20%"}} />
          <div>
             <h3>{authorName}</h3>
             <a href={authorLink}></a> 
@@ -17,27 +17,31 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["uncooked macaroni",
+   "butter",
+   "flour",
+   "heavy cream",
+   "milk"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
          <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
+            {ingredients.map((ingredient, index) => {
+               return <li key={index}>{ingredient}</li>
+            })}
          </ul>
       </div>
    );
 }
 
 const RecipeDescription = () => {
+   let recipeName = "Stovetop Mac and Cheese"
+   let recipeDescription = "This Creamy Stovetop Mac and Cheese is the BEST 30-minute meal for a busy week. Your family will love it every time!"
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>{recipeName}</h1>
+            <p>{recipeDescription}</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -49,7 +53,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://thecozycook.com/wp-content/uploads/2021/10/Stovetop-Mac-and-Cheese-2.jpg" alt="" className="imageUpdates"/>
    );
 }
 
